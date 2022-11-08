513a7dda6ce08cf16ca23afb4882457658a7d6fc
Alexander McCarter <AlexanderMcCarter@outlook.com>
2022-11-08 16:36:14 -0500

    Graphical updates to the main menu, to make it more consistent.

--- script/ui/mainMenu.gd
+++ script/ui/mainMenu.gd
@@ -2,15 +2,20 @@ extends Control
 
 var swoopSpeed = 4
 
+var swoopTopY = -300
+var swoopLowY = 720+300
+
 func _ready():
 	# Cause all the UI elements to swoop into the scene from outside the screen,
 	# to their original positions. (Making it so that they go to where they are
 	# means that I don't have to do the work to figure out where I want them,
 	# I can just move them around and it will automatically work.)
-	$Title.swoop(Vector2(0, -300), $Title.rect_position, swoopSpeed)
-	$PlayButton.swoop(Vector2($PlayButton.rect_position.x, -300), $PlayButton.rect_position, swoopSpeed)
-	$ChangeIconButton.swoop(Vector2($ChangeIconButton.rect_position.x, 720+300), $ChangeIconButton.rect_position, swoopSpeed)
-	$HighScore.swoop(Vector2($HighScore.rect_position.x, -300), $HighScore.rect_position, swoopSpeed)
+	
+	$Title.swoop(Vector2(0, swoopTopY), $Title.rect_position, swoopSpeed)
+	$PlayButton.swoop(Vector2($PlayButton.rect_position.x, swoopTopY), $PlayButton.rect_position, swoopSpeed)
+	
+	$ChangeIconButton.swoop(Vector2($ChangeIconButton.rect_position.x, swoopLowY), $ChangeIconButton.rect_position, swoopSpeed)
+	$HighScore.swoop(Vector2($HighScore.rect_position.x, swoopLowY), $HighScore.rect_position, swoopSpeed)
 	
 	# Set the high score text TO the high score
 	$HighScore/Score.set_text(str(Global.data["highScore"]))
